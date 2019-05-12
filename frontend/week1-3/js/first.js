5329789487aaa44c3f9de21fe8a9bd4ae957b858
yingyingyingyin <18852892723@163.com>
2019-05-12 19:06:04 +0800

    six

---
+++ frontend/week1-3/js/first.js
@@ -0,0 +1,37 @@
+function up() {
+    var a = document.getElementById('jump');
+    a.style.display = 'block';
+};
+
+function down() {
+    var a = document.getElementById('jump');
+    a.style.display = 'none';
+};
+
+function up1() {
+    var a = document.getElementById('div1');
+    a.style.display = 'block';
+   
+};
+
+function down1() {
+    var a = document.getElementById('div1');
+    a.style.display = 'none';
+    
+};
+function up2(){
+    var b=document.getElementById('div2');
+    b.style.display='block';
+}
+function down2(){
+    var b=document.getElementById('div2');
+    b.style.display='none';
+}
+function up3(){
+    var b=document.getElementById('div3');
+    b.style.display='block';
+}
+function down3(){
+    var b=document.getElementById('div3');
+    b.style.display='none';
+}
