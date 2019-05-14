81a5884addc0a7cf8c2d47bfe85d22906b6840c8
Cardy <1124804361@qq.com>
2019-05-14 19:00:04 +0800

    a little fix

--- python/task_01/DuranceX/function2.py
+++ python/task_01/DuranceX/function2.py
@@ -1,23 +1,6 @@
-#直接在D盘建立一个“test.txt”文件
-path="D:\\"
-full_path=path+'test.txt'
-file=open(full_path,'w')
-a='''一张褪色的照片，
-    好像带给我一点点怀念。
-    巷尾老爷爷卖的热汤面，
-    味道弥漫过旧旧的后院；
-    流浪猫睡熟在摇晃秋千，
-    夕阳照了一遍他咪着眼；
-    那张同桌寄的明信片，
-    安静的躺在课桌的里面。'''
-file.write(a)
-file.close()
-file=open(full_path,'r')
-file.read()
-file.close()
-file=open(full_path,'r')
-file.readline()
-file.close
-with open(full_path,'r') as file:
-		file.readlines()
+def list_sum(*x):
+	sum1=0
+	for number in x:
+		sum1=sum1+number
+	return sum1
 	
